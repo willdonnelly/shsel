8b73c921caf0c3b01218f962959366952b095c59
Will Donnelly <will.donnelly@gmail.com>
2009-12-15 17:17:56 -0600

    I know it shouldn't be important, but: 100 lines!

--- shsel.c
+++ shsel.c
@@ -4,12 +4,7 @@
 #include <unistd.h>
 #include <pwd.h>
 
-/* read_shells - process the shells file and assemble a list of options
- *   each line of the file will be of the format:
- *       <user> <shell name> /path/to/shell --arguments
- *   the first shell listed for the user will be the default
- *   users may be interspersed in any order
- */
+/* read_shells - read data from the shells file for the current user */
 int read_shells (char* shells_path, char ***shell_name, char ***shell_cmds) {
     FILE *shells_file = fopen(shells_path, "r");
     if(!shells_file) {
@@ -52,13 +47,10 @@ int read_shells (char* shells_path, char ***shell_name, char ***shell_cmds) {
     return num_shells;
 }
 
-/* read_choice - read an integer from the user
- *   if it's out of bounds [0, num_shells) we return 0
- */
+/* read_choice - read an integer from the user and clip to [0, num_shells) */
 int read_choice (int num_shells) {
     char *choice_str = calloc(65, sizeof(char));
     fgets(choice_str, 64, stdin);
-
     if(choice_str[0] == '\n') {
         free(choice_str);
         return 0;
@@ -82,7 +74,6 @@ int main (int argc, char** argv) {
     char **shell_name  = 0;
     char **shell_cmds  = 0;
     num_shells = read_shells("/etc/shsel", &shell_name, &shell_cmds);
-
     if(num_shells == 0) {
         printf("no shells for you -- go away\n");
         exit(1);
